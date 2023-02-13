6e9a737398d1307a36eb43e9f3f77a87932193ae
makkahwi <suhaibahmadai@hotmail.com>
2023-02-13 22:42:05 +0300

    Add & Engage CurrencyEdition Model

---
+++ src/api/currency-edition/controllers/currency-edition.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * currency-edition controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::currency-edition.currency-edition');
---
+++ src/api/currency-edition/routes/currency-edition.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * currency-edition router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::currency-edition.currency-edition');
