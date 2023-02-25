16779ebce3faffa5159be998120ed2f49e3a51d1
makkahwi <suhaibahmadai@hotmail.com>
2023-02-25 20:34:50 +0300

    DB Backup Up To Pieces

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 "use strict";
-// const list = require("../database/seeds/countries.json");
-// const codes = require("../database/seeds/countries.json");
+const list = require("../database/seeds/currenciesList.json");
+const editions = require("../database/seeds/editions.json");
 
 module.exports = {
   /**
@@ -22,12 +22,40 @@ module.exports = {
   bootstrap(/*{ strapi }*/) {},
 
   // For creating entries
-  // async bootstrap({ strapi }) {
-  //   await strapi.db.query("api::country.country").createMany({
-  //     data: list.map((ele) => ({
-  //       ...ele,
-  //       mapCode: codes.find((c) => c.name === ele.name)?.code.toLowerCase(),
-  //     })),
-  //   });
-  // },
+  async bootstrap({ strapi }) {
+    // const final = list.reduce(
+    //   (final, current) => [
+    //     ...final,
+    //     ...Object.keys(current)
+    //       ?.filter(
+    //         (key) => key.includes("collected") || key.includes("toCollect")
+    //       )
+    //       .map((key) => ({
+    //         value: current[key],
+    //         type: "Banknote",
+    //         currency_edition: editions.find(
+    //           (edition) => edition.currency.code === current.code
+    //         )?.id,
+    //       }))
+    //       .sort((a, b) => a.value - b.value),
+    //   ],
+    //   []
+    // );
+    // let i = 0;
+    // let timer = setInterval(async () => {
+    //   if (i < final.length) {
+    //     console.log(i);
+    //     await strapi.db.query("api::piece.piece").create({
+    //       data: final[i++],
+    //     });
+    //   } else {
+    //     clearInterval(timer);
+    //   }
+    // }, 1500);
+    // await strapi.db.query("api::piece.piece").create({
+    //   data: list.map((ele) => ({
+    //     ...ele,
+    //   })),
+    // });
+  },
 };
